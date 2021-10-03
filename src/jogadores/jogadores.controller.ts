f33afa49ed1b7b69d3e88b1e9079c6f2b75ca747
Thiago Coppi <thiago.coppi@premiersoft.net>
2021-10-02 22:26:02 -0300

    Criação metodos crud

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,17 +1,35 @@
 import { JogadoresService } from './jogadores.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { Jogador } from './interfaces/jogadores.interface';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
 
-    
-    constructor(private readonly jogadoresService: JogadoresService) {}
+
+    constructor(private readonly jogadoresService: JogadoresService) { }
 
     @Post()
     async criarAtualizarJogador(
         @Body() criarJogadorDto: CriarJogadorDto) {
-            await this.jogadoresService.criarAtualizarJogador(criarJogadorDto);
+        await this.jogadoresService.criarAtualizarJogador(criarJogadorDto);
+    }
+
+    @Get()
+    async consultarJogadores(
+        @Query('email') email: string
+    ): Promise<Jogador[] | Jogador> {
+        if (email) {
+            return await this.jogadoresService.consultarJogadorPeloEmail(email);
         }
+        return this.jogadoresService.consultarTodosJogadores();
+    }
+
+    @Delete()
+    async deletarJogador(
+        @Query('email') email: string
+    ): Promise<void> {
+        return this.jogadoresService.removerJogador(email);
+    }
 
 }
--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -1,19 +1,44 @@
 import { Jogador } from './interfaces/jogadores.interface';
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 
 @Injectable()
-export class JogadoresService {
+export class JogadoresService {    
     private readonly logger = new Logger(JogadoresService.name);
     private jogadores: Jogador[] = [];
+
     async criarAtualizarJogador(criarJogadorDto: CriarJogadorDto): Promise<void> {
-        this.criar(criarJogadorDto);
+        const { email } = criarJogadorDto;
+        const jogadorEncontrado = this.jogadores.find(x => x.email === email);
+
+        if (!jogadorEncontrado) {
+            this.criar(criarJogadorDto);
+            return;
+        }
+
+        this.atualizar(jogadorEncontrado, criarJogadorDto);
     }
-    
+
+    async removerJogador(email: string): Promise<void> {
+        const jogadorEncontrado = this.jogadores.find(x => x.email === email);
+        this.jogadores = this.jogadores.filter(x => x.email !== jogadorEncontrado.email);
+    }
+
+    async consultarJogadorPeloEmail(email: string): Promise<Jogador> {
+        var jogadorEncontrado = this.jogadores.find(x => x.email === email);
+        if (!jogadorEncontrado) {
+            throw new NotFoundException(`O jogador com o e-mail ${email} não foi encontrado`);
+        }
+
+        return jogadorEncontrado;
+    }
+
     private criar(criarJogadorDto: CriarJogadorDto): void {
         const { nome, email, telefoneCelular } = criarJogadorDto;
+
+
         const jogador: Jogador = {
             _id: uuidv4(),
             nome,
@@ -26,6 +51,15 @@ export class JogadoresService {
         this.logger.log(`Criando jogador: ${JSON.stringify(jogador)}`);
 
         this.jogadores.push(jogador);
+    }
+
+    private atualizar(jogador: Jogador, criarJogadorDto: CriarJogadorDto): void {
+        jogador.nome = criarJogadorDto.nome;
+
+    }
+
+    async consultarTodosJogadores(): Promise<Jogador[]> {
+        return this.jogadores;
     }
 
 }
